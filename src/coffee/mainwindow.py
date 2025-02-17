69f1ff61a00e36f026a1d090fbb6ee7e071abc07
ym12s <ym12snake@gmail.com>
2025-02-17 18:57:25 +0700

    Update All

--- src/coffee/mainwindow.py
+++ src/coffee/mainwindow.py
@@ -1,24 +1,20 @@
 import sys
-from PySide6.QtWidgets import (QMainWindow, QFrame,
-                               QVBoxLayout, QWidget,
-                               QGraphicsDropShadowEffect, QButtonGroup)
-from PySide6.QtGui import QColor, QIcon
-from __ui.ui_form import Ui_MainWindow
+import json
+sys.stdout.reconfigure(encoding="utf-8")
+from PySide6.QtWidgets import (QMainWindow, QButtonGroup)
+from PySide6.QtGui import QIcon
+from src.__ui.ui_mainwindow import Ui_MainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.setWindowIcon(QIcon("anh/icon.png"))
+        self.setWindowIcon(QIcon("src/__ass/icon.ico"))
         self.setWindowTitle("Quản lí cà phê")
 
-        self.shadow = QGraphicsDropShadowEffect()
-        self.shadow.setBlurRadius(20)
-        self.shadow.setXOffset(5)
-        self.shadow.setYOffset(5)
-        self.shadow.setColor(QColor(0, 0, 0, 180))
-        self.ui.frame.setGraphicsEffect(self.shadow)
+        self.load_styles()  # Gọi đúng hàm
+
         self.buttons = [self.ui.btnHome, self.ui.btnMenu,
                         self.ui.btnDonHang, self.ui.btnKho,
                         self.ui.btnNhanvien, self.ui.btnKhachHang, self.ui.btnOder]
@@ -27,5 +23,22 @@ class MainWindow(QMainWindow):
         for btn in self.buttons:
             btn.setCheckable(True)
             self.buttonGroup.addButton(btn)
-            self.buttons[0].setChecked(False)
+        self.buttons[0].setChecked(False)
+
+    def load_styles(self):
+        try:
+            with open("src/__ui/style.json", "r", encoding="utf-8") as f:
+                styles = json.load(f)
+                css = "\n".join([f"{selector} {{ {rules} }}" for selector, rules in styles.items()])
+                self.setStyleSheet(css)
+        except Exception as e:
+            print("Lỗi khi load CSS:", e)
+
+
+            # self.shadow = QGraphicsDropShadowEffect()
+            # self.shadow.setBlurRadius(20)
+            # self.shadow.setXOffset(5)
+            # self.shadow.setYOffset(5)
+            # self.shadow.setColor(QColor(0, 0, 0, 180))
+            # self.ui.frame.setGraphicsEffect(self.shadow)
 
